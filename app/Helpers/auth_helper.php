1de2a8d8e1ef9be4b5dfa5a4ed10c693d43a1d7b
Mikhan-TMP <mikhanbalbastro@outlook.com>
2025-05-23 09:27:47 +0800

    fixed some bugs in auth_helper

--- app/Helpers/auth_helper.php
+++ app/Helpers/auth_helper.php
@@ -1,20 +1,15 @@
 <?php
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-use CodeIgniter\HTTP\ResponseInterface;
-    function authorizationCheck(){
-        $authHeader = $this->request->getHeaderLine('Authorization');
-        $token = str_replace('Bearer ', '', $authHeader);
-        if (!$token) {
-            return null; 
-        }
-        try {
-            $decoded = JWT::decode($token, new Key(getenv('JWT_SECRET'), 'HS256'));
-            return $decoded->user_id ?? null;
-        } catch (\Exception $e) {
-            return null;
-        }
+function authorizationCheck($request)
+{
+    $authHeader = $request->getHeaderLine('Authorization');
+    $token = str_replace('Bearer ', '', $authHeader);
+    if (!$token) {
+        return null; 
     }
-
-
-?>
+    try {
+        $decoded = \Firebase\JWT\JWT::decode($token, new \Firebase\JWT\Key(getenv('JWT_SECRET'), 'HS256'));
+        return $decoded->user_id ?? null;
+    } catch (\Exception $e) {
+        return null;
+    }
+}
